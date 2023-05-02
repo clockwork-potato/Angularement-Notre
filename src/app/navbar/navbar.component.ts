ad1a264433d8b3a3566249158e323a92489cd419
Emestay <103575914+Emestay@users.noreply.github.com>
2023-05-02 11:01:13 +0200

    thththth

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  dropdownOpen = false;
+  mobileMenuOpen = false;
+  title = "JvaisTangular"
+}
