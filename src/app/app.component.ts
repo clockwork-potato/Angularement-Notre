991352f33dc1bb9064ec5655b92a2eb934f52dd4
Emestay <103575914+Emestay@users.noreply.github.com>
2023-05-02 10:20:35 +0200

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'JvaisTangular';
+  
+  constructor(private router: Router) {}
+
+  navigateToFeatures() {
+    this.router.navigate(['/features']);
+  }
+
+  navigateToContact() {
+    this.router.navigate(['/contact']);
+  }
 }
+
