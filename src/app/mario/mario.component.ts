95a6e5662507461cdc349d7ff4a4099d7e1b9752
Emestay <103575914+Emestay@users.noreply.github.com>
2023-05-03 14:41:37 +0200

    [Mario]

--- src/app/mario/mario.component.ts
+++ src/app/mario/mario.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./mario.component.css']
 })
 export class MarioComponent {
+  randomMario: string = 'https://source.unsplash.com/random/300x300?Mario';
+
   userName: string = 'Mario';
   userAge: number = 25;
   userFavNumber: number = Math.ceil(Math.random() * 100 );
