6438e87b074b0ceaa5a0d6ab7d58403dca0e4a3c
Emestay <103575914+Emestay@users.noreply.github.com>
2023-05-04 11:36:36 +0200

    push du tailwind sur mon fetch() et ajout du typage et interface TS

--- src/app/fetch/fetch.component.ts
+++ src/app/fetch/fetch.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+interface WeatherData {
+  hourly: {
+    temperature_2m: number[];
+  };
+  latitude: number;
+  longitude: number;
+}
+
 @Component({
   selector: 'app-fetch',
   templateUrl: './fetch.component.html',
@@ -7,28 +15,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FetchComponent implements OnInit  {
   temperatures: number[] = [];
+  lat: string ='';
  
   ngOnInit(): void {
     this.fetchWeatherData();
   }
-
   fetchWeatherData(): void {
-    
     const apiUrl: string = `https://api.open-meteo.com/v1/forecast?latitude=43.60&longitude=1.44&hourly=temperature_2m`;
-    
+
     fetch(apiUrl)
-      .then((r) => {
-        if (r.ok) {
-          return r.json();
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
         } else {
           throw new Error('Erreur lors de la récupération des données météo');
         }
       })
-      .then((data) => {
+      .then((data: WeatherData) => {
         console.log(data);
         this.temperatures = data.hourly.temperature_2m;
-        
-
+        this.lat = `Latitude: ${data.latitude} Longitude: ${data.longitude}`;
       })
       .catch((error) => {
         console.error('Erreur lors de la récupération des données météo:', error);
