170bcf3402b4e9c23ab4948e4810baac346617ec
Emestay <103575914+Emestay@users.noreply.github.com>
2023-05-11 10:28:49 +0200

    PUSSSSSHHHH

--- src/app/truc/bouffe.service.ts
+++ src/app/truc/bouffe.service.ts
@@ -6,9 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class BouffeService {
-  constructor(private http: HttpClient) {}
+  constructor(private _http: HttpClient) {}
 
   getRandomMeal(): Observable<any> { 
-    return this.http.get('https://www.themealdb.com/api/json/v1/1/random.php');
+    return this._http.get('https://www.themealdb.com/api/json/v1/1/random.php');
   }
 }
